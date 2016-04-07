5fab38f21cb326a60c5f47a20bc9e805ec9bfe89
tomclarkson <athleticartistry@hotmail.com>
2016-04-07 14:32:29 +1200

    test module exports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-export default class Test extends Component {
-	render() {
-		return (
-			<h1>Test works!</h1>
-		);
-	}
-}
+// export default class Test extends Component {
+// 	render() {
+// 		return (
+// 			<h1>Test works!</h1>
+// 		);
+// 	}
+// }
+
+module.exports = 'B4';
